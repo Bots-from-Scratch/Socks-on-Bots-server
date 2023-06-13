69839e5ced9718dba222f552e10390bed3310bed
Stefan <mohrst84031@th-nuernberg.de>
2023-06-13 17:21:37 +0200

    Add cors origin render

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require("express")();
 const http = require("http").Server(app);
 const io = require("socket.io")(http,{
     cors: {
-        origin: [ "https://vueonbots.onrender.com/"]
+        origin: ["https://socksonbots.de", "https://vueonbots.onrender.com"]
     }
 });
 // const path = require("path");
